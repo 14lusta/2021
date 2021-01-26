bd6ea50aaa2b201e1a18d31ef7c3df4d9e99e46b
L BOX <64248606+14lusta@users.noreply.github.com>
2021-01-26 11:51:35 +0100

    Picture
    This program makes Graphics Without Importing any thing

---
+++ Picture.java
@@ -0,0 +1,45 @@
+public class Start {
+    public static void main(String args[]) {
+      System.out.println("  __________");
+      System.out.println(" / Hello   /");
+      System.out.println("|         |");
+      System.out.println("|World.   |");
+      System.out.println("/_________/");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("(traditional text whith new Graphics)");
+      System.out.println();
+      System.out.println("------------------------------------------------------------------");
+// Pic    
+      System.out.println();
+      System.out.println("How to make Graphics in java whith NO imports...");
+      System.out.println();
+      System.out.println();
+      System.out.println();
+      System.out.println("|_____________________________________________________________________________|");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");
+      System.out.println("|              ____                  ___                                      |");
+      System.out.println("|      |    | |       |      |      |   |                                     |");
+      System.out.println("|      |____| |____   |      |      |   |                                     |");
+      System.out.println("|      |    | |       |      |      |   |                                     |");
+      System.out.println("|      |    | |____   |_____ |____  |___|                                     |");
+      System.out.println("|                        ____    ____              _____                      |");
+      System.out.println("|            |       /  |    |  |    |  |         |     |                     |");
+      System.out.println("|            |  /|  /   |    |  |____|  |         |     |                     |");
+      System.out.println("|            | / | /    |    |  |^      |         |     |                     |");
+      System.out.println("|            |/  |/     |____|  | ^     |_____    |____/                      |");
+      System.out.println("|                                  ^                                          |");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");
+      System.out.println("|                                                                             |");      
+      System.out.println("|_____________________________________________________________________________|");
+
+                         
+    }
+}
